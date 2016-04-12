a46bf593b547ad9f9bda47382b892828b25f6c8b
airhop <airhop12@gmail.com>
2016-04-12 02:25:09 -0600

    Update searchjavascript.js

--- grocery/public/js/searchjavascript.js
+++ grocery/public/js/searchjavascript.js
@@ -6,127 +6,6 @@ $("form").submit(function (e) {
            var myQuery = document.getElementById("inputbox").value;
       var urls ="https://api2.bigoven.com/recipes?title_kw="+myQuery+"&pg=1&rpp=20&api_key=2MX8tWBt2B3U5VWChEngoRCT4dnHx8Uf";
       document.getElementById("inputbox").value = "";
-      var urls2 ="https://api2.bigoven.com/recipes?title_kw="+myQuery+"&pg=2&rpp=20&api_key=2MX8tWBt2B3U5VWChEngoRCT4dnHx8Uf";
-      var urls3="https://api2.bigoven.com/recipes?title_kw="+myQuery+"&pg=3&rpp=20&api_key=2MX8tWBt2B3U5VWChEngoRCT4dnHx8Uf";
-      var pic="pic";
-      var fancypic="fancypic";
-      var link="link";
-      var number=1;
-      var title="text";
-$.ajax({
-        url: urls,
-          dataType: 'json',
-          success: function( data ){
-           number=1;
-if(data['Results'].length==0)
-{
-  alert("Invalid Search");
-  location.reload();
-}
-}});
-for(var number=1; number<17; number++)
-{
-             var currentpic=pic+number;
-             var currentrecipe=title+number;
-             var currentfancypic=fancypic+number;
-             var currentlink=link+number;
-             document.getElementById(currentpic).src="";
-             document.getElementById(currentpic).style.width="261px";
-             document.getElementById(currentpic).style.height="269px";
-             document.getElementById(currentrecipe).innerHTML="";
-             document.getElementById(currentfancypic).href="";
-   
-   }          document.getElementById(currentlink).href="";
-
-
-      $.ajax({
-        url: urls,
-          dataType: 'json',
-          success: function( data ){
-           number=1;
-          $.each(data['Results'], function(name2, value)
-          {
-
-           if(number<17&&value.PhotoUrl!="http://photos.bigoven.com/recipe/hero/recipe-no-image.jpg")
-           {
-             var currentpic=pic+number;
-             var currentrecipe=title+number;
-             var currentfancypic=fancypic+number;
-             var currentlink=link+number;
-             console.log(value.PhotoUrl);
-             document.getElementById(currentpic).src=value.PhotoUrl;
-             document.getElementById(currentpic).style.width="261px";
-             document.getElementById(currentpic).style.height="269px";
-             document.getElementById(currentrecipe).innerHTML=value.Title;
-             document.getElementById(currentfancypic).href=value.PhotoUrl;
-             document.getElementById(currentlink).href=value.WebURL;
-             number++;
-           }
-          });
-
-
-        $.ajax({
-        url: urls2,
-        dataType: 'json',
-        success:function(data){  
-                var loop=0;     
-          $.each(data['Results'], function(name2, value)
-          {
-
-            loop++;
-           if(number<17&&value.PhotoUrl!="http://photos.bigoven.com/recipe/hero/recipe-no-image.jpg")
-           {
-             var currentpic=pic+number;
-             var currentrecipe=title+number;
-             var currentfancypic=fancypic+number;
-             var currentlink=link+number;
-             console.log(value.PhotoUrl);
-             document.getElementById(currentpic).src=value.PhotoUrl;
-             document.getElementById(currentpic).style.width="261px";
-             document.getElementById(currentpic).style.height="269px";
-             document.getElementById(currentrecipe).innerHTML=value.Title;
-             document.getElementById(currentfancypic).href=value.PhotoUrl;
-             document.getElementById(currentlink).href=value.WebURL;
-             number++;
-           }
-           else if(loop==number&&number!=17)
-           {
-             var currentpic=pic+number;
-             var currentrecipe=title+number;
-             var currentfancypic=fancypic+number;
-             var currentlink=link+number;
-             console.log(value.PhotoUrl);
-             document.getElementById(currentpic).src=value.PhotoUrl;
-             document.getElementById(currentpic).style.width="261px";
-             document.getElementById(currentpic).style.height="269px";
-             document.getElementById(currentrecipe).innerHTML=value.Title;
-             document.getElementById(currentfancypic).href=value.PhotoUrl;
-             document.getElementById(currentlink).href=value.WebURL;
-             number++;
-           }
-
-          });
-
-
- }
-
-      })
-
-
-
-
-          }
-
-}
-    )
-        });
-
-    $("#icon").click(
-    function(e){     
-
-     var myQuery = document.getElementById("inputbox").value;
-      var urls ="https://api2.bigoven.com/recipes?title_kw="+myQuery+"&pg=1&rpp=20&api_key=2MX8tWBt2B3U5VWChEngoRCT4dnHx8Uf";
-      document.getElementById("inputbox").value = "";
 //
       $.ajax({
         url: urls,
@@ -137,6 +16,12 @@ for(var number=1; number<17; number++)
            var link="link";
            var number=1;
            var title="text";
+           if(data['Results'].length==0)
+           {
+            console.log("Failure");
+            alert("Invalid Search Please Try Again");
+            location.reload();
+           }
           $.each(data['Results'], function(name2, value)
           {
            if(number<17)
@@ -155,35 +40,42 @@ for(var number=1; number<17; number++)
            }
 
           });
-          $.ajax({
-        url: urls2,
-        dataType: 'json',
-        success:function(data){  
-        var loop=1;     
+          
+          }
+
+}
+    )
+        });
+
+    $("#icon").click(
+    function(e){     
+     var myQuery = document.getElementById("inputbox").value;
+      var urls ="https://api2.bigoven.com/recipes?title_kw="+myQuery+"&pg=1&rpp=20&api_key=2MX8tWBt2B3U5VWChEngoRCT4dnHx8Uf";
+      document.getElementById("inputbox").value = "";
+//
+      $.ajax({
+        url: urls,
+          dataType: 'json',
+          success: function( data ){
+           var pic="pic";
+           var fancypic="fancypic";
+           var link="link";
+           var number=1;
+           var title="text";
+           if(data['Results'].length==0)
+           {
+            console.log("Failure");
+            alert("Invalid Search Please Try Again");
+            location.reload();
+           }
           $.each(data['Results'], function(name2, value)
           {
-           if(number<17&&value.PhotoUrl!="http://photos.bigoven.com/recipe/hero/recipe-no-image.jpg")
+           if(number<17)
            {
              var currentpic=pic+number;
              var currentrecipe=title+number;
              var currentfancypic=fancypic+number;
              var currentlink=link+number;
-             console.log(value.PhotoUrl);
-             document.getElementById(currentpic).src=value.PhotoUrl;
-             document.getElementById(currentpic).style.width="261px";
-             document.getElementById(currentpic).style.height="269px";
-             document.getElementById(currentrecipe).innerHTML=value.Title;
-             document.getElementById(currentfancypic).href=value.PhotoUrl;
-             document.getElementById(currentlink).href=value.WebURL;
-             number++;
-           }
-            else if(loop==number&&number!=17)
-           {
-             var currentpic=pic+number;
-             var currentrecipe=title+number;
-             var currentfancypic=fancypic+number;
-             var currentlink=link+number;
-             console.log(value.PhotoUrl);
              document.getElementById(currentpic).src=value.PhotoUrl;
              document.getElementById(currentpic).style.width="261px";
              document.getElementById(currentpic).style.height="269px";
@@ -194,12 +86,6 @@ for(var number=1; number<17; number++)
            }
 
           });
-
-
-
- }
-
-      })
           
           }
 
