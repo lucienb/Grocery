f5607dcb2163b527e8687bc906077bb7e8a16298
Lucien <lucien.briscoe@gmail.com>
2016-04-12 14:42:48 +0000

    got pretty good headwaym but didnt get all the way. Removing from the database is an issue, and users' info doesnt autopopulate :/

--- grocery/public/js/main.js
+++ grocery/public/js/main.js
@@ -1,9 +1,10 @@
-jQuery(function($) {'use strict';
+jQuery(function($) {
+	'use strict';
 
 	//Responsive Nav
-	$('li.dropdown').find('.fa-angle-down').each(function(){
-		$(this).on('click', function(){
-			if( $(window).width() < 768 ) {
+	$('li.dropdown').find('.fa-angle-down').each(function() {
+		$(this).on('click', function() {
+			if ($(window).width() < 768) {
 				$(this).parent().next().slideToggle();
 			}
 			return false;
@@ -14,31 +15,33 @@ jQuery(function($) {'use strict';
 	new WOW().init();
 
 	// recipeSearch filter
-	$(window).load(function(){
+	$(window).load(function() {
 
 		$('.main-slider').addClass('animate-in');
 		$('.preloader').remove();
 		//End Preloader
 
-		if( $('.masonery_area').length ) {
-			$('.masonery_area').masonry();//Masonry
+		if ($('.masonery_area').length) {
+			$('.masonery_area').masonry(); //Masonry
 		}
 
 		var $portfolio_selectors = $('.recipeSearch-filter >li>a');
-		
-		if($portfolio_selectors.length) {
-			
+
+		if ($portfolio_selectors.length) {
+
 			var $recipeSearch = $('.recipeSearch-items');
 			$recipeSearch.isotope({
-				itemSelector : '.recipeSearch-item',
-				layoutMode : 'fitRows'
+				itemSelector: '.recipeSearch-item',
+				layoutMode: 'fitRows'
 			});
-			
-			$portfolio_selectors.on('click', function(){
+
+			$portfolio_selectors.on('click', function() {
 				$portfolio_selectors.removeClass('active');
 				$(this).addClass('active');
 				var selector = $(this).attr('data-filter');
-				$recipeSearch.isotope({ filter: selector });
+				$recipeSearch.isotope({
+					filter: selector
+				});
 				return false;
 			});
 		}
@@ -47,12 +50,13 @@ jQuery(function($) {'use strict';
 
 
 	$('.timer').each(count);
+
 	function count(options) {
 		var $this = $(this);
 		options = $.extend({}, options || {}, $this.data('countToOptions') || {});
 		$this.countTo(options);
 	}
-		
+
 	// Search
 	$('.fa-search').on('click', function() {
 		$('.field-toggle').fadeToggle(200);
@@ -61,17 +65,23 @@ jQuery(function($) {'use strict';
 
 
 	// Progress Bar
-	$.each($('div.progress-bar'),function(){
-		$(this).css('width', $(this).attr('data-transition')+'%');
-	});       
+	$.each($('div.progress-bar'), function() {
+		$(this).css('width', $(this).attr('data-transition') + '%');
+	});
 
 });
+
+var userName = 'User';
+var userRef;
+var allUserInfo = null;//User Array. 2 Variables contained in it. Format: Array GROCERYLIST[], key, items. Array WEEKRECIPES[], Day of Week, key, items.
+var weekrecipes = [];
 
 function openLogin() {
 	$('#homePageDiv').hide();
 	$('#planPageDiv').hide();
 	$('#grocerySearchDiv').hide();
 	$('#recipeSearchDiv').hide();
+	$('#likedRecipes').hide();
 	$('#normalHeaderDiv').hide();
 	$('#loginHeaderDiv').show();
 	$('#loginDiv').show();
@@ -83,8 +93,10 @@ function openHome() {
 	$('#grocerySearchDiv').hide();
 	$('#recipeSearchDiv').hide();
 	$('#loginHeaderDiv').hide();
+	$('#likedRecipes').hide();
 	$('#normalHeaderDiv').show();
 	$('#homePageDiv').show();
+	$('.userName').html(userName);
 }
 
 function openPlan() {
@@ -95,6 +107,9 @@ function openPlan() {
 	$('#loginHeaderDiv').hide();
 	$('#normalHeaderDiv').show();
 	$('#planPageDiv').show();
+	$('#likedRecipes').show();
+
+	//print weekly recipes
 }
 
 function openGrocery() {
@@ -103,8 +118,11 @@ function openGrocery() {
 	$('#planPageDiv').hide();
 	$('#recipeSearchDiv').hide();
 	$('#loginHeaderDiv').hide();
+	$('#likedRecipes').hide();
 	$('#normalHeaderDiv').show();
 	$('#grocerySearchDiv').show();
+
+	printGroceries();
 }
 
 function openRecipe() {
@@ -115,35 +133,151 @@ function openRecipe() {
 	$('#loginHeaderDiv').hide();
 	$('#normalHeaderDiv').show();
 	$('#recipeSearchDiv').show();
+	$('.idLabel').hide();
+	$('#likedRecipes').show();
 }
 
+function printGroceries() {
+	$('#outputcontent').html('');
+	for (i = 0; i < groceries.length; i++) {
+		var item;
+		item = groceries[i];
+		var htmlstring = '<li class="btn btn-lg btn-primary groceryItem">' + item + "</li>";
+		$('#outputcontent').append(htmlstring);
+	}
+	post_to_db_func();
+}
+
+function removeFromArray(itemToRemove, arrayName) {
+	var indexToRemove = jQuery.inArray(itemToRemove, arrayName);
+	if (indexToRemove > -1) {
+		arrayName.splice(indexToRemove, 1);
+	};
+}
+
+function login() {
+	userName = $('#inputName').val();
+
+	if (userName.length === 0) {
+		alert('Please Enter a username...');
+	} 
+
+	else {
+		var userFound = 0; 
+//		console.log(userFound);														//allUserInfo = [array in database]
+	
+		if (userFound === 0) {
+			console.log("User not in database...yet.")
+			openHome();
+		}
+		else {
+			console.log("User Found in Database");
+
+			openPlan();
+		}
+	}
+	post_to_db_func();
+}
 
 $(document).ready(function() {
 	openLogin();
 
-    $('#autocomplete').on("keypress", function(e) {
-        if (e.keyCode == 13) {
-        	  var ingredient = $('#autocomplete').val();
-		      var thehtml = '<li class="btn btn-lg btn-primary groceryItem">' + ingredient + "</li>";
-		      $('#outputcontent').append(thehtml);
-		      //Push ingredient into JS groceries Array
-		      $('#autocomplete').val('');
-		      return false;
-        }
+
+	$('#inputName').on("keypress", function(e) {
+		if (e.keyCode == 13) {
+			login();
+		}
+	});
+
+	$('#autocomplete').on("keypress", function(e) {
+		if (e.keyCode == 13) {
+			if ($('#autocomplete').val().length !== 0) {
+				var ingredient = $('#autocomplete').val();
+				groceries.push(ingredient);
+				post_to_db_func();
+
+				printGroceries();
+				$('#autocomplete').val('');
+			}
+		}
 	});
 	$('#grocerySearchIcon').on("click", function(e) {
-    	  var ingredient = $('#autocomplete').val();
-	      var thehtml = '<li class="btn btn-lg btn-primary groceryItem">' + ingredient + "</li>";
-	      $('#outputcontent').append(thehtml);
-	      //Push ingredient into JS groceries Array
-	      $('#autocomplete').val('');
-	      console.log(groceries);
+		if ($('#autocomplete').val().length !== 0) {
+			var ingredient = $('#autocomplete').val();
+			groceries.push(ingredient);
+			post_to_db_func();
+
+			printGroceries();
+			$('#autocomplete').val('');
+		}
 	});
 
 	$('#outputcontent').on('click', '.groceryItem', function() {
 		var thisItem = $(this).text();
-		//Search JS groceries array for 'thisItem', remove it
+		removeFromArray(thisItem, groceries);
+		post_to_db_func();
+
+		//Search Mongo Array, remove item from database
+
 		$(this).remove();
 	});
+	$('#likedRecipes').on('click', '.removeLiked', function() {
+		var thisItem = this.parentNode.outerHTML;
+		console.log(likedRecipes);
+		removeFromArray(thisItem, likedRecipes);
+		$(this).closest('.likedRecipe').remove();
+		console.log(likedRecipes);
+	});
+
+	$('[data-toggle="tooltip"]').tooltip();
+
 
 });
+
+function post_to_db_func(){
+  var myobj = {UserName: userName, GroceryList: groceries, WeeklyRecipes: weekrecipes};
+  jobj = JSON.stringify(myobj);
+  console.log("JSON String: " + jobj)
+
+  var url = "user";
+
+    $.ajax({
+      url:url,
+      type: "POST",
+      data: jobj,
+      contentType: "application/json; charset=utf-8",
+      success: function(data,textStatus) {
+//             $("#done").html("The User was Successfully Posted.");
+      }
+    });
+};
+
+function remove_from_db_func(){
+      console.log(userName + " to be Removed")
+
+      var url = "remove";
+
+        $.ajax({
+          url:url,
+          type: "POST",
+          data: userName,
+          contentType: "application/json; charset=utf-8",
+          success: function(data,textStatus) {
+//             $("#done").html("The User was Successfully Posted.");
+          }
+        });
+};
+
+function get_users() {
+  $.getJSON('user', function(data) {
+    console.log(data);
+    var numUsers = data.length;
+    console.log("numUsers: " + numUsers);
+
+    allUserInfo = data; 
+
+    
+
+//  $("#allusers").html(everything);
+  })
+};
